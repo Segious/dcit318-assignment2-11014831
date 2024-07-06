ce6589bdc72da4dd0eddb31ba60c7dc1dd634269
Segious Kotoku <skotoku001@st.ug.edu.gh>
2024-07-06 00:41:54 +0000

    Completed inheritance

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        Console.WriteLine("Animal makes:");
+        animal.MakeSound();
+
+        Console.WriteLine("\nDog makes:");
+        dog.MakeSound();
+
+        Console.WriteLine("\nCat makes:");
+        cat.MakeSound();
+    }
+}
